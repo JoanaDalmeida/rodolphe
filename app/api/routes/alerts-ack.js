a5611976a7f600de3bdd5091c67cb0d7decbeabe
Pierre <pierre.besson7@gmail.com>
2015-02-06 21:39:01 +0100

    [app]Update node API.

--- app/api/routes/alerts-ack.js
+++ app/api/routes/alerts-ack.js
@@ -1,7 +1,8 @@
 var data = require('../data');
-
 module.exports = function(request, reply){
   var alert = JSON.parse(request.payload);
+  console.log(alert.id);
+  console.log(request.params.id);
   if(request.params.id !== alert.id){
     throw new Error("L'identifiant de l'url n'est pas le même que celui de l'objet envoyé.");
   }

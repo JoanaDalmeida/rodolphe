a401714d18c8ca19d9d500a16a21b97d2b571860
Pierre <pierre.besson7@gmail.com>
2015-02-06 22:08:46 +0100

    [api] node change port for deployment.

--- app/api/index.js
+++ app/api/index.js
@@ -3,7 +3,7 @@ var printRoutes = require('./util/print-routes');
 var routes = require('./routes');
 var config = { connections: { routes: { cors: true } } }; //, docs: true };
 var server = new Hapi.Server(config);
-server.connection({ port: 8080 });
+server.connection({ port: parseInt(process.env.PORT) || 8080 });
 server.route(routes);
 server.start(function() {
   console.log('Server started at: ' + server.info.uri);

fa4b5936222b4f16dbf0bd0e095c25320d202958
Pierre <pierre.besson7@gmail.com>
2015-02-16 07:19:14 +0100

    [app] Add search component initialization.

---
+++ app/app/component/search/input.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var SearchInputMixin = {
+  getDefaultProps: function(){
+    return {
+      placeholder: 
+    }
+  }
+  render:function renderSearchInput(){
+
+  }
+};
+
+module.exports = {
+  mixin: SearchInputMixin,
+  component: React.createClass(SearchInputMixin)
+};
